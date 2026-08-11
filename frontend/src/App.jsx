a18aeb5c6ed97c7acaceed89e318758ac7ea66f4
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-11 23:21:05 +0530

    initial checking of the rendering of the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-
 import './App.css'
+import {BrowserRouter  as Router, Routes, Route} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
+
+import Login from './pages/Auth/Login.jsx'
+import SignUp from './pages/Auth/SignUp.jsx'
+import LandingPage from './pages/LandingPage.jsx';
+import Dashboard from './pages/Home/Dashboard.jsx'
+import InterviewPrep from './pages/InterviewPrep/InterviewPrep.jsx'
 
 function App() {
 
 
   return (
     <>
-     <div className='text-blue-400'>Yashas INTERVIEW prep AI</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* {Default Route} */}
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signUp' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/interview-prep/:sessionId' element={<InterviewPrep/>}/>
+        </Routes>
+      </Router>
+
+      <Toaster 
+      toastOptions={{
+        className:"",
+        style:{
+          fontSize: '13px',
+        }
+      }}/>
+    </div>
     </>
   )
 }
