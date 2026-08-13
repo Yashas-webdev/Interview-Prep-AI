8d1c67b142f4e29c3368a0f57bcae85f2f69e7b7
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-13 23:59:17 +0530

    added the style for the first three card

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -78,26 +78,26 @@ function LandingPage() {
         </div>
       
 
-      <div className="">
-        <div className="">
-          <section className="">
-            <h2 className="">
+      <div className="w-full min-h-full bg-[-#FFFCEF] mt-10">
+        <div className="container mx-auto px-4 pt-10 pb-20">
+          <section className="mt-5">
+            <h2 className="text-2xl font-medium text-center mb-12">
               Features That Make you Shine
             </h2>
 
-            <div className="">
+            <div className="flex flex-col items-center gap-8">
               {/* First 3 cards */}
-              <div className="">
+              <div className="grid grid-col-1 md:grid-cols-3 gap-8 w-full">
                 {APP_FEATURES.slice(0,3).map((feature)=>(
                   <div 
                      key = {feature.id}
-                     className=""
+                     className="bg-[#FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
                   >
-                    <h3 className="">
+                    <h3 className="text-base font-semibold mb-3">
                       {feature.title}
                     </h3>
 
-                    <p className="">{feature.descritpion}</p>
+                    <p className="text-gray-600">{feature.descritpion}</p>
                   </div>
                 ))}
               </div>
