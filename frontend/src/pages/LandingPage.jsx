a12dd9e0c0ea47b4641578df7b80bdee07201cec
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-14 00:11:43 +0530

    added the style for the remaining two cards

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -97,23 +97,23 @@ function LandingPage() {
                       {feature.title}
                     </h3>
 
-                    <p className="text-gray-600">{feature.descritpion}</p>
+                    <p className="text-gray-600">{feature.description}</p>
                   </div>
                 ))}
               </div>
 
               {/* Remaining 2 cards */}
-              <div className="">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 {APP_FEATURES.slice(3).map((feature) => (
                   <div 
                      key = {feature.id}
-                     className=""
+                     className="bg-[FFFEF8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
                   >
-                    <h3 className="">
+                    <h3 className="text-base font-semibold mb-3">
                       {feature.title}
                     </h3>
 
-                    <p className="">{feature.description}</p>
+                    <p className="text-gray-600">{feature.description}</p>
                   </div>
                 ))}
               </div>
@@ -122,7 +122,7 @@ function LandingPage() {
         </div>
       </div>
 
-      <div className="">
+      <div className="texxt-sm bg-gray-50 text-secondary text-center p-5 mt-5">
         Made by 😎... Yashas Coding
       </div>
     </div>
