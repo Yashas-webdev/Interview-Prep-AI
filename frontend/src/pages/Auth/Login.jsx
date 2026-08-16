a194409e5a442e5bd0a5a907a3e2716e10adb696
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-16 19:19:37 +0530

    created the login form

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,48 @@
 // import React from 'react'
 
-function Login() {
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom'
+
+
+const Login = ({setCurrentpage}) => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //Handle Login Form Submit 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter you details to log in
+      </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({target})=>setEmail(target.value)}
+          label = "Email Address"
+          placeholder = 'jonhn@example.com'
+          type= 'text'
+        />
+
+        <Input
+          value={password}
+          onChange={({target})=>setPassword(target.value)}
+          label = "Password"
+          placeholder = 'Min 8 characters'
+          type = "password"
+        />
+        
+      </form>
+    </div>
   )
 }
 
