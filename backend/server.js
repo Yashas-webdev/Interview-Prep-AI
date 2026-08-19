fbd8c864bdf6115ad13cbd1d53baf2b398fdb8d5
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-19 13:49:44 +0530

    created the backend required folder strcuture

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+
+//Midleware to handle CORS
+app.use(
+    cors({
+        origin:'*',
+        methods:['GET','POST','PUT',"DELETE"],
+        allowedHeader: ["Content-Type",'Authorization']
+    })
+);
+
+//Midleware
+app.use(express.json());
+
+//Routes
+
+//Serve uploads folder
+app.use("/uploads",express.static(path.join(__dirname,'uploads'),{}));
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
