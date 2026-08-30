6397093c3e0a413f77dd655a886abf56c445b88b
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-30 23:05:45 +0530

    created the userContext and created fetchUser and updateUser function

---
+++ frontend/src/context/userContext.jsx
@@ -0,0 +1,51 @@
+import  {createContext, useState, useEffect, Children} from "react";
+import axiosInstance from "../utils/axiosInstance.js"
+import { API_PATHS } from "../utils/apiPaths.js";
+
+export const UserContext = createContext();
+
+const UserProvider = ({Children}) => {
+
+const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(true); // New state to track loading
+
+useEffect( () => {
+    if (user) return;
+
+    const accessToken = localStorage.getItem("token");
+    if(!accessToken) {
+        setLoading(false);
+        return;
+    }
+
+    const fetchUser = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE);
+            setUser (response.data);
+        } catch (error) {
+            console.error("User not authenticated",error);
+            clearUser();
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchUser();
+}, []);
+
+
+    const updateUser = (userData) => {
+        setUser(userData);
+        localStorage.setItem("token",userData.toekn);//Save tken
+    }
+
+
+    return (
+        <UserContext.Provider value={{user, loading, updateUser, clearUser}}>
+            {Children}
+        </UserContext.Provider>
+    );
+};
+
+
+export default UserProvider;
